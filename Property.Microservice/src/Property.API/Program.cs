c55463b6e6e62c05253c2abb0f075fd145f2e392
Christian Nanyim <nanyim.alain@gmail.com>
2025-09-13 10:55:21 +0200

    feat: configure backend to accept camelCase JSON properties

--- Property.Microservice/src/Property.API/Program.cs
+++ Property.Microservice/src/Property.API/Program.cs
@@ -13,7 +13,12 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
